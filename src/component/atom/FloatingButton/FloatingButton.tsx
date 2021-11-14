f1417d12b9e878bd78ac6f069048beb54235f30a
mpl1018 <marcoarmona@gmail.com>
2021-11-14 13:32:57 +0100

    Button animation

--- src/component/atom/FloatingButton/FloatingButton.tsx
+++ src/component/atom/FloatingButton/FloatingButton.tsx
@@ -21,24 +21,25 @@ export default class FloatingButton extends Component<Props, state> {
         this.setState({ clicked: !this.state.clicked });
         const btn = document.querySelector(".btn-icon");
         btn.classList.toggle("active");
+        const incidenceButton = document.querySelector(".floating-incidence");
+        const postButton = document.querySelector(".floating-post");
+        incidenceButton.classList.toggle("incidence-animation");
+        postButton.classList.toggle("post-animation");
     }
 
     render(): JSX.Element {
         const { clicked } = this.state;
         return (
             <>
-                {clicked && (
-                    <>
-                        <Button className="floating-incidence">
-                            <span className="btn-text">CREAR INCIDENCIA</span>
-                            <Icon icon="whiteIncidentIcon" className="inline-incidence-icon"></Icon>
-                        </Button>
-                        <Button className="floating-post">
-                            <span className="btn-text">CREAR PUBLICACIÓN</span>
-                            <Icon icon="whitePostIcon" className="inline-incidence-icon"></Icon>
-                        </Button>
-                    </>
-                )}
+                <Button className="floating-incidence">
+                    <span className="btn-text">CREAR INCIDENCIA</span>
+                    <Icon icon="whiteIncidentIcon" className="inline-incidence-icon"></Icon>
+                </Button>
+                <Button className="floating-post">
+                    <span className="btn-text">CREAR PUBLICACIÓN</span>
+                    <Icon icon="whitePostIcon" className="inline-incidence-icon"></Icon>
+                </Button>
+
                 <div className="floating-button" onClick={() => this.onButtonClick()}>
                     <Icon icon="buttonIcon" className="btn-icon"></Icon>
                 </div>
