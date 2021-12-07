0c4764db75d3a17bf249938d8e4b182a80f9e588
Alex <alex@neoxelox.com>
2021-12-07 01:43:35 +0100

    Add dockerfile

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="react-scripts" />
 declare let process: {
     env: {
-        FREYJA_ODIN_BASE_URL: string;
+        REACT_APP_FREYJA_ODIN_BASE_URL: string;
     };
 };
--- src/services/api/OpenApiConfig.ts
+++ src/services/api/OpenApiConfig.ts
@@ -2,4 +2,4 @@ import { TokenSecureStorage } from "../storage/TokenSecureStorage";
 import { OpenAPI } from "./core/OpenAPI";
 
 OpenAPI.TOKEN = async () => (await new TokenSecureStorage().getToken()) || "";
-OpenAPI.BASE = process.env.FREYJA_ODIN_BASE_URL || "";
+OpenAPI.BASE = process.env.REACT_APP_FREYJA_ODIN_BASE_URL || "";
