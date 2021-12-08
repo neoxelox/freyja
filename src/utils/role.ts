c3dab0b34790e0c07c4d8cf2e1a207e65c0bd8be
Alex <alex@neoxelox.com>
2021-12-08 21:44:57 +0100

    Changes so people don't get mad

--- src/utils/role.ts
+++ src/utils/role.ts
@@ -1,9 +1,9 @@
 import { InvitationDto } from "../services/model/invitation.dto";
 
 export function role(role: InvitationDto["role"]): string {
-    if (role === "ADMINISTRATOR") return "Administrador";
-    else if (role === "SECRETARY") return "Secretario";
-    else if (role === "LESSEE") return "Alquilado";
-    else if (role === "PRESIDENT") return "Presidente";
-    else if (role === "RESIDENT") return "Vecino";
+    if (role === "ADMINISTRATOR") return "Administrador/a";
+    else if (role === "SECRETARY") return "Secretario/a";
+    else if (role === "LESSEE") return "Alquilado/a";
+    else if (role === "PRESIDENT") return "Presidente/a";
+    else if (role === "RESIDENT") return "Vecino/a";
 }
